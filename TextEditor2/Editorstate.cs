971ef82db09e58423a969b5a52710f0a2258bc54
nielsdeG <niels.de.gier@gmail.com>
2025-06-06 11:38:36 +0200

    Added undo + redo, and fixes for downarrow, uparrow and delete

---
+++ TextEditor2/Editorstate.cs
@@ -0,0 +1,27 @@
+﻿namespace TextEditor2;
+
+public class Editorstate
+{
+    public List<String> Lines
+    {
+        get;
+        set;
+    }
+    public int LineRows
+    {
+        get;
+        set;
+    }
+    public int LineColumns
+    {
+        get;
+        set;
+    }
+
+    public Editorstate(List<string> lines, int lineRows, int lineColumns)
+    {
+        this.Lines = new List<String>(lines); 
+        this.LineRows = lineRows;
+        this.LineColumns = lineColumns;
+    }
+}
--- TextEditor2/Program.cs
+++ TextEditor2/Program.cs
@@ -1,25 +1,47 @@
 ﻿namespace TextEditor2;
 public class Program
 {
+    private static Stack<Editorstate> UndoStack = new Stack<Editorstate>();
+    private static Stack<Editorstate> RedoStack = new Stack<Editorstate>();
+
+    
+    private static List<String> lines = new List<String> {"123"};
+        
+    private static int lineColumns = 0;
+    private static int lineRows = 0;
+
     public static void Main(string[] args)
     {
         Console.CursorVisible = true;
         
-        List<String> lines = new List<String> {"123"};
+        
+        
 
-        int lineColumns = 0;
-        int lineRows = 0;
         
         while (true) {
             Console.Clear();
             for (int i = 0; i < lines.Count; i++)
             {
-            Console.WriteLine(lines[i]);
+                Console.WriteLine(lines[i]);
             }
             
             Console.SetCursorPosition(lineColumns, lineRows);
             
              ConsoleKeyInfo key = Console.ReadKey(true);
+
+             if (key.Modifiers == ConsoleModifiers.Control)
+             {
+                 if (key.Key == ConsoleKey.Z)
+                 {
+                     Undo();
+                 }
+
+                 if (key.Key == ConsoleKey.Y)
+                 {
+                     Redo();
+                 }
+             }
+             
              switch (key.Key)
              {
                  case ConsoleKey.LeftArrow:
@@ -30,40 +52,110 @@ public class Program
                      if (lines[lineRows].Length > lineColumns)
                          lineColumns++;
                      break;
+                 
                  case ConsoleKey.Enter:
-                     lines.Insert(lineRows + 1, "");
+                     UndoStack.Push(new Editorstate(lines, lineRows, lineColumns));
+                     RedoStack.Clear();
+                     String afterCusor = "";
+                     if (lineColumns < lines[lineRows].Length)
+                     {
+                         afterCusor = lines[lineRows].Substring(lineColumns);
+                         lines[lineRows] = lines[lineRows].Substring(0, lineColumns);
+                     }
+                     lines.Insert(lineRows + 1, afterCusor);
                      lineColumns = 0;
                      lineRows++;
                      break;
+                 
                  case ConsoleKey.UpArrow:
-                     if(lineRows > 0)
-                        lineRows--;
+                     if (lineRows > 0)
+                     {
+                         lineRows--;
+                         lineColumns = Math.Min(lineColumns, lines[lineRows].Length);
+                     }
                      break;
+                 
                  case ConsoleKey.DownArrow:
-                     if (lineRows < lines.Count -1)
+                     if (lineRows < lines.Count - 1)
+                     {
                          lineRows++;
+                         lineColumns = Math.Min(lineColumns, lines[lineRows].Length);
+                     }
                      break;
+                 
                  case ConsoleKey.Backspace:
                      if (lineColumns > 0)
                      {
+                         UndoStack.Push(new Editorstate(lines, lineRows, lineColumns));
+                         RedoStack.Clear();
                          lines[lineRows] = lines[lineRows].Remove(lineColumns -1, 1);
                          lineColumns--;
+                     } else if (lineRows > 0)
+                     {
+                         UndoStack.Push(new Editorstate(lines, lineRows, lineColumns));
+                         RedoStack.Clear();
+                         String line = lines[lineRows]; 
+                         lines.RemoveAt(lineRows);
+                         lineRows--;
+                         lineColumns = lines[lineRows].Length;
+                         lines[lineRows] += line;
                      }
                      break;
+                 
                  case ConsoleKey.Delete:
-                     if (lineColumns > 0 && lineColumns < lines[lineRows].Length)
-                     lines[lineRows] = lines[lineRows].Remove(lineColumns, 1);
+                     if (lineColumns < lines[lineRows].Length)
+                     {
+                         UndoStack.Push(new Editorstate(lines, lineRows, lineColumns));
+                         RedoStack.Clear();
+                         lines[lineRows] = lines[lineRows].Remove(lineColumns, 1);
+                     }else if (lineRows < lines.Count - 1)
+                     {
+                         UndoStack.Push(new Editorstate(lines, lineRows, lineColumns));
+                         RedoStack.Clear();
+                         lines[lineRows] += lines[lineRows + 1];
+                         lines.RemoveAt(lineRows + 1);
+                     }
+
                      break;
 
                  default:
                      if (key.KeyChar >= ' ' && key.KeyChar <= '~' || char.IsLetterOrDigit(key.KeyChar))
                      {
+                         UndoStack.Push(new Editorstate(lines, lineRows, lineColumns));
                          lines[lineRows] = lines[lineRows].Insert(lineColumns, key.KeyChar.ToString());
                          lineColumns++;
+                         RedoStack.Clear();
                      }
                      break;
              }
         }
-
     }
+
+    private static void Undo()
+    {
+        if (UndoStack.Count > 0)
+        {
+            Editorstate undoState = UndoStack.Peek();
+            UndoStack.Pop();
+            RedoStack.Push(new Editorstate(lines, lineRows, lineColumns));
+            lines = new List<string>(undoState.Lines);
+            lineRows = undoState.LineRows;
+            lineColumns = undoState.LineColumns;
+            
+        }
+    }
+    
+    private static void Redo()
+    {
+        if (RedoStack.Count > 0)
+        {
+            Editorstate redoState = RedoStack.Peek();
+            RedoStack.Pop();
+            UndoStack.Push(new Editorstate(lines, lineRows, lineColumns));
+            lines = new List<string>(redoState.Lines);
+            lineRows = redoState.LineRows;
+            lineColumns = redoState.LineColumns;
+        }
+    }
+    
 }
